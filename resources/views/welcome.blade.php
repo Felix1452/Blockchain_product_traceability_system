8ff9b6930dd239002a68d04933009c3e1b2b1dd0
Phuc <115924067+Phuchuynh1452@users.noreply.github.com>
2023-09-18 17:24:54 +0700

    Finish Setup QR code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,7 @@
             <h2>Simple QR Code</h2>
         </div>
         <div class="card-body">
-
-                        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate('https://techvblogs.com/blog/generate-qr-code-laravel-9')); !!}">
+            <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate('https://techvblogs.com/blog/generate-qr-code-laravel-9')); !!}">
         </div>
     </div>
 
